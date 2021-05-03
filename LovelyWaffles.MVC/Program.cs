a4209c46523131f8c5911712632d7a5f407489ad
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-05-03 23:14:52 +0300

    DI services and seeding data.

--- LovelyWaffles.MVC/Program.cs
+++ LovelyWaffles.MVC/Program.cs
@@ -1,11 +1,7 @@
+using LovelyWaffles.MVC.Models;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LovelyWaffles.MVC
 {
@@ -13,7 +9,12 @@ namespace LovelyWaffles.MVC
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host=CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                WaffleDbSeeder.Seed(scope.ServiceProvider);
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
