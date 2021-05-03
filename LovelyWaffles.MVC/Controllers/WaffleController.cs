782fa919001cbdea78cd5da5b082b7a934cf96e8
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-05-04 00:49:27 +0300

    Create functionality added.

--- LovelyWaffles.MVC/Controllers/WaffleController.cs
+++ LovelyWaffles.MVC/Controllers/WaffleController.cs
@@ -17,5 +17,21 @@ namespace LovelyWaffles.MVC.Controllers
             var waffles = _context.Waffles.ToList();
             return View(waffles);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Waffle waffle)
+        {
+            var waffleId = _context.Waffles.Select(w => w.WaffleId).Max() + 1;
+            waffle.WaffleId = waffleId;
+            _context.Waffles.Add(waffle);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
