8ab182edc2e9f8e10509c517f8ee7279dd01105a
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-05-04 00:10:24 +0300

    Controller and Waffle view added

---
+++ LovelyWaffles.MVC/Controllers/WaffleController.cs
@@ -0,0 +1,21 @@
+﻿using LovelyWaffles.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace LovelyWaffles.MVC.Controllers
+{
+    public class WaffleController : Controller
+    {
+        private readonly WaffleDbContext _context;
+
+        public WaffleController(WaffleDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var waffles = _context.Waffles.ToList();
+            return View(waffles);
+        }
+    }
+}
