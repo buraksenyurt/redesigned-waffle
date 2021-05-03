4c8274c6afc5bd6ad4158f8f9ac49ed8b4964145
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-05-03 22:59:24 +0300

    Model and DbContext created.

---
+++ LovelyWaffles.MVC/Models/Waffle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace LovelyWaffles.MVC.Models
+{
+    public class Waffle
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<string> Ingradients { get; set; }
+        public bool InStock { get; set; }
+        public string Recipe { get; set; }
+    }
+}
---
+++ LovelyWaffles.MVC/Models/WaffleDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LovelyWaffles.MVC.Models
+{
+    public class WaffleDBContext
+        :DbContext
+    {
+        public WaffleDBContext(DbContextOptions<WaffleDBContext> options)
+            :base(options)
+        {
+        }
+        public DbSet<Waffle> Waffles { get; set; }
+    }
+}
