c563f264eb8a126d9cea1b1e88556d604c590c65
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-05-05 00:21:07 +0300

    Change HTML helpers to Tag Helpers

--- LovelyWaffles.RazorPages/Data/Waffle.cs
+++ LovelyWaffles.RazorPages/Data/Waffle.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace LovelyWaffles.RazorPages.Data
 {
     public class Waffle
     {
+        [Display(Name ="No")]
         public int WaffleId { get; set; }
         public string Title { get; set; }
+        [Display(Name ="On Sold")]
         public bool InStock { get; set; }
         public string Recipe { get; set; }
     }
